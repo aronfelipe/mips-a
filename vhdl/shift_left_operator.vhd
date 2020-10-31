acc02befd9243b0380351e34fd274e98644a5e89
felipe <aronakamoto@outlook.com>
2020-10-31 10:16:29 -0300

    Feat: Continue

--- vhdl/shift_left_operator.vhd
+++ vhdl/shift_left_operator.vhd
@@ -7,18 +7,18 @@ entity shift_left_operator is
 	);
 	
 	port (
-		data_in : in std_logic_vector(15 downto 0);
+		data_in : in std_logic_vector(data_width - 1 downto 0);
 		data_out : out std_logic_vector(data_width - 1  downto 0)
-	)
+	);
 	
 end entity;
 
-architecture rtl shift_left_operator is 
+architecture rtl of shift_left_operator is 
 
 	signal shift : std_logic_vector(data_width - 1 downto 0);
 	
 begin 
-convert (data_width - 1 downto 0) <= data_in (29 downto 0) & "00";
+shift (data_width - 1 downto 0) <= data_in (29 downto 0) & "00";
 data_out <= shift;
 
 end architecture;
